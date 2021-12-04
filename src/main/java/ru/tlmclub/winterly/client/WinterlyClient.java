087b79391da3054f9a481f02739a29e88c9bf6b3
PinkGoosik <MCGycuk@yandex.ru>
2021-12-05 01:08:20 +0500

    client entrypoint

---
+++ src/main/java/ru/tlmclub/winterly/client/WinterlyClient.java
@@ -0,0 +1,11 @@
+package ru.tlmclub.winterly.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WinterlyClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
