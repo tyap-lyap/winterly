26fc362c66c7ddeaa6cc60fca89da4e223fcce90
PinkGoosik <MCGycuk@yandex.ru>
2021-12-04 23:58:33 +0500

    use bclib model for present

--- src/main/java/ru/tlmclub/winterly/block/PresentBlock.java
+++ src/main/java/ru/tlmclub/winterly/block/PresentBlock.java
@@ -1,14 +1,25 @@
 package ru.tlmclub.winterly.block;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.client.render.model.json.JsonUnbakedModel;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
+import org.jetbrains.annotations.Nullable;
+import ru.bclib.blocks.BaseBlock;
+import ru.bclib.client.models.ModelsHelper;
+import ru.bclib.client.models.PatternsHelper;
+import ru.tlmclub.winterly.data.WinterlyPatterns;
+
+import java.util.Optional;
 
 @SuppressWarnings("deprecation")
-public class PresentBlock extends Block {
+public class PresentBlock extends BaseBlock {
     public static final VoxelShape SHAPE = Block.createCuboidShape(2, 0, 2, 14, 14, 14);
 
     public PresentBlock(Settings settings) {
@@ -18,5 +29,19 @@ public class PresentBlock extends Block {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return SHAPE;
+    }
+
+//    @Environment(EnvType.CLIENT)
+//    @Override
+//    public JsonUnbakedModel getItemModel(Identifier blockId) {
+//        return this.getBlockModel(blockId, this.getDefaultState());
+//    }
+
+    @Environment(EnvType.CLIENT)
+    @Nullable
+    @Override
+    public JsonUnbakedModel getBlockModel(Identifier blockId, BlockState blockState) {
+        Optional<String> pattern = PatternsHelper.createJson(WinterlyPatterns.PRESENT, blockId);
+        return ModelsHelper.fromPattern(pattern);
     }
 }
---
+++ src/main/java/ru/tlmclub/winterly/data/WinterlyPatterns.java
@@ -0,0 +1,8 @@
+package ru.tlmclub.winterly.data;
+
+import net.minecraft.util.Identifier;
+import ru.tlmclub.winterly.WinterlyMod;
+
+public class WinterlyPatterns {
+    public static final Identifier PRESENT = WinterlyMod.newId("patterns/block/present.json");
+}
