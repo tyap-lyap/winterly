d0e28f37576f7e9a437fc46a8a9262eb9b8b9904
aSomik <asdburn@mail.ru>
2022-01-09 01:00:58 +0300

    bunch of food items

--- src/main/java/ru/tlmclub/winterly/item/EdibleItem.java
+++
@@ -1,9 +0,0 @@
-package ru.tlmclub.winterly.item;
-
-import net.minecraft.item.Item;
-
-public class EdibleItem extends Item {
-    public EdibleItem(Settings settings) {
-        super(settings);
-    }
-}
