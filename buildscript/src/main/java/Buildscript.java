0d8e67742aa6b93817c8408e4e9f6266eb0497b9
PinkGoosik <pinkgoosik@vk.com>
2022-11-18 21:20:42 +0500

    brachyura

---
+++ buildscript/src/main/java/Buildscript.java
@@ -0,0 +1,105 @@
+import io.github.coolcrabs.brachyura.decompiler.BrachyuraDecompiler;
+import io.github.coolcrabs.brachyura.decompiler.fernflower.FernflowerDecompiler;
+import io.github.coolcrabs.brachyura.fabric.*;
+import io.github.coolcrabs.brachyura.fabric.FabricContext.ModDependencyCollector;
+import io.github.coolcrabs.brachyura.fabric.FabricContext.ModDependencyFlag;
+import io.github.coolcrabs.brachyura.maven.Maven;
+import io.github.coolcrabs.brachyura.maven.MavenId;
+import io.github.coolcrabs.brachyura.minecraft.Minecraft;
+import io.github.coolcrabs.brachyura.minecraft.VersionMeta;
+import io.github.coolcrabs.brachyura.quilt.QuiltMaven;
+import io.github.coolcrabs.brachyura.processing.ProcessorChain;
+import net.fabricmc.mappingio.tree.MappingTree;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Buildscript extends SimpleFabricProject {
+
+	@Override
+	public String getModId() {
+		return Properties.MOD_ID;
+	}
+
+	@Override
+	public String getVersion() {
+		return Properties.MOD_VERSION;
+	}
+
+	@Override
+	public VersionMeta createMcVersion() {
+		return Minecraft.getVersion(Properties.MINECRAFT);
+	}
+
+	@Override
+	public MappingTree createMappings() {
+		return Yarn.ofMaven(FabricMaven.URL, FabricMaven.yarn(Properties.YARN_MAPPINGS)).tree;
+	}
+
+	@Override
+	public FabricLoader getLoader() {
+		return new FabricLoader(FabricMaven.URL, FabricMaven.loader(Properties.FABRIC_LOADER));
+	}
+
+	@Override
+	public void getModDependencies(ModDependencyCollector d) {
+		addFabricModules(d);
+
+		d.addMaven("https://maven.shedaniel.me", new MavenId("me.shedaniel.cloth", "cloth-config-fabric", "8.2.88"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		d.addMaven("https://maven.shedaniel.me", new MavenId("me.shedaniel.cloth", "basic-math", "0.6.1"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "modmenu", "4.0.6"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+
+		d.addMaven("https://ladysnake.jfrog.io/artifactory/mods", new MavenId("dev.onyxstudios.cardinal-components-api", "cardinal-components-api", "5.0.2"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		d.addMaven("https://ladysnake.jfrog.io/artifactory/mods", new MavenId("dev.onyxstudios.cardinal-components-api", "cardinal-components-base", "5.0.2"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		d.addMaven("https://ladysnake.jfrog.io/artifactory/mods", new MavenId("dev.onyxstudios.cardinal-components-api", "cardinal-components-entity", "5.0.2"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "trinkets", "3.4.0"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "emi", "0.4.2+1.19"), ModDependencyFlag.RUNTIME);
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "lazydfu", Properties.LAZY_DFU), ModDependencyFlag.RUNTIME);
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "ferrite-core", "5.0.0-fabric"), ModDependencyFlag.RUNTIME);
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "starlight", "1.1.1+1.19"), ModDependencyFlag.RUNTIME);
+		d.addMaven("https://api.modrinth.com/maven/", new MavenId("maven.modrinth", "lithium", "mc1.19.2-0.10.2"), ModDependencyFlag.RUNTIME);
+	}
+
+	public static void addFabricModules(ModDependencyCollector d) {
+		d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", "fabric-api", Properties.FABRIC_API), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		try {
+			String temp = "https://maven.fabricmc.net/net/fabricmc/fabric-api/fabric-api/%version%/fabric-api-%version%.pom";
+			String pom = temp.replaceAll("%version%", Properties.FABRIC_API);
+			URL url = new URL(pom);
+			URLConnection request = url.openConnection();
+			request.connect();
+			InputStreamReader isReader = new InputStreamReader(request.getInputStream());
+			MavenXpp3Reader reader = new MavenXpp3Reader();
+			Model model = reader.read(isReader);
+
+			model.getDependencies().forEach(dependency -> {
+				var id = dependency.getArtifactId();
+				var ver = dependency.getVersion();
+				d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", id, ver), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+			});
+
+		}
+		catch(Exception e) {
+			System.out.println("Failed to add fabric modules due to an exception:\n" + e);
+		}
+	}
+
+	@Override
+	public int getJavaVersion() {
+		return 17;
+	}
+
+	@Override
+	public BrachyuraDecompiler decompiler() {
+		return new FernflowerDecompiler(Maven.getMavenJarDep(QuiltMaven.URL, new MavenId("org.quiltmc", "quiltflower", Properties.QUILTFLOWER)));
+	}
+
+	@Override
+	public ProcessorChain resourcesProcessingChain() {
+		return new ProcessorChain(super.resourcesProcessingChain(), new FmjVersionFixer(this));
+	}
+}
---
+++ buildscript/src/main/java/Properties.java
@@ -0,0 +1,12 @@
+public class Properties {
+	public static final String MOD_ID = "winterly";
+	public static final String MOD_VERSION = "0.7.2";
+
+	public static final String MINECRAFT = "1.19.2";
+	public static final String FABRIC_LOADER = "0.14.10";
+	public static final String FABRIC_API = "0.64.0+1.19.2";
+	public static final String YARN_MAPPINGS = "1.19.2+build.28";
+	public static final String QUILTFLOWER = "1.9.0";
+
+	public static final String LAZY_DFU = "0.1.3";
+}
